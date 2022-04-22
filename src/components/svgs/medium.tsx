941379eac71a673501a67f3f733b60c7fd3739ca
Stackbit Code Editor <projects@stackbit.com>
2022-04-22 09:35:56 +0000

    medium.tsx: changed

--- src/components/svgs/medium.tsx
+++ src/components/svgs/medium.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 export default function Medium({ className }) {
     return (
-        <svg className={className} viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><title/><rect height="500" rx="55.43" width="500" x="49.89" y="49.89"/>
+        <svg className={className} viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><title/><rect height="412" rx="55.43" width="500" x="49.89" y="49.89"/>
 <circle cx="197.51" cy="256" fill="#fff" r="77.45"/>
 <ellipse cx="318.97" cy="255.56" fill="#fff" rx="35.07" ry="72.01"/><ellipse cx="378.21" cy="255.56" fill="#fff" rx="13.73" ry="65.08"/></svg>
       
